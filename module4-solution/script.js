074faa3826038ac51b991fd7aafa6783469e8230
NagaSaiJyothi-soumithri <ninika.jyothi@gmail.com>
2020-08-03 17:59:31 +0530

    .

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,11 +1,10 @@
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim","joe","pranu","jack"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 
 for (var i = 0; i < names.length; i++) {
 
-  
   var firstLetter = names[i].charAt(0).toLowerCase();
 
   
