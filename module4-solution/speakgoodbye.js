916d8d9dc9b46d81ade5564d9e92cb05654b7bf0
NagaSaiJyothi-soumithri <ninika.jyothi@gmail.com>
2020-08-03 18:33:23 +0530

    .

--- module4-solution/speakgoodbye.js
+++ module4-solution/speakgoodbye.js
@@ -1,15 +1,9 @@
-(function(window) {
-
-
-  var byeSpeaker = {};
-  var speakWord = "Good Bye";
   
-
-
-byeSpeaker.speak = function (name) {
-  console.log(speakWord + " " + name);
-}
-
- 
-window.byeSpeaker = byeSpeaker;
+(function(window) {
+    var byeSpeaker = new Object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function speak(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
 })(window);
